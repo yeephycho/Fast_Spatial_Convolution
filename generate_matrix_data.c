121d5ffd59bdde2ebedf9b80770a016b711f42fe
huyixuan <huyixuan@huyixuans-MacBook-Pro.local>
2016-10-14 23:56:29 +0800

    With data alignment and plain old convolution operation

---
+++ generate_matrix_data.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+int main(){
+    // Allocate aligned memory here.
+    void* mem_ptr = malloc(sizeof(float)*256*256 + 15);
+    float* input_data_ptr = ((uintptr_t)mem_ptr + 15) & ~ (uintptr_t)0x0F;
+    printf("Matrix started at address: %p, this address divides 16 is: %lu\n", input_data_ptr, (size_t)input_data_ptr / 16);
+
+    // Data initialization here,
+    for(register int i = 0; i < 256*256; ++i){
+        input_data_ptr[i] = 1.0f;
+    //    printf("%d: %f\n", i, input_data_ptr[i]);
+    }
+
+    float conv_kernel[9] = {3.0f, 3.0f, 3.0f,
+                            3.0f, 3.0f, 3.0f,
+                            3.0f, 3.0f, 3.0f};
+
+    float output_data_ptr[254*254];
+
+    // f_mm_c(input_data_ptr, conv_kernel, output_data_ptr);
+    for(int i = 0; i < 254; i++){
+        for(int j = 0; j < 254; j++){
+           output_data_ptr[i*254 + j] = input_data_ptr[i*256 + j] * conv_kernel[0];
+           output_data_ptr[i*254 + j] += input_data_ptr[i*256 + j + 1] * conv_kernel[1];
+           output_data_ptr[i*254 + j] += input_data_ptr[i*256 + j + 2] * conv_kernel[2];
+           output_data_ptr[i*254 + j] += input_data_ptr[(i+1)*256 + j] * conv_kernel[3];
+           output_data_ptr[i*254 + j] += input_data_ptr[(i+1)*256 + j + 1] * conv_kernel[4];
+           output_data_ptr[i*254 + j] += input_data_ptr[(i+1)*256 + j + 2] * conv_kernel[5];
+           output_data_ptr[i*254 + j] += input_data_ptr[(i+2)*256 + j] * conv_kernel[6];
+           output_data_ptr[i*254 + j] += input_data_ptr[(i+2)*256 + j + 1] * conv_kernel[7];
+           output_data_ptr[i*254 + j] += input_data_ptr[(i+2)*256 + j + 2] * conv_kernel[8];
+        }
+    }
+    
+    for(register int i = 0; i < 254*254; ++i){
+        printf("%d: %f\n", i, output_data_ptr[i]);
+    }
+
+
+    // Free allocated memory here.
+    free(mem_ptr);
+    mem_ptr = NULL;
+
+    return 0;
+}
